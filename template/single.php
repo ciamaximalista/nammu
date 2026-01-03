4db17014de968c6ef002f0efe975e3e676bca784
maximalista <compania@maximalista.coop>
2026-01-03 18:17:34 +0100

    Actualiza presentación

--- template/single.php
+++ template/single.php
@@ -686,7 +686,7 @@ if ($isPageTemplate && $formattedDate !== '') {
         }
         .site-search-form input[type="text"],
         .site-search-form input[type="email"] {
-            flex: 1 1 80px;
+            flex: 0 1 60px;
             width: auto;
             min-width: 0;
             padding: 0.25rem 0.35rem;
